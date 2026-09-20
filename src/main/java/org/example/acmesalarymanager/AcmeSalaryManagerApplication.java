040790406167a4967f446456d84a66672ad40f9e
Sanjana Aithal <sanjanaaithal@Sanjanas-MacBook-Air.local>
2026-09-20 12:32:21 +0530

    first commit

---
+++ src/main/java/org/example/acmesalarymanager/AcmeSalaryManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.acmesalarymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeSalaryManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmeSalaryManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/acmesalarymanager/AcmeSalaryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.acmesalarymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeSalaryManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
